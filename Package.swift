e278f51379a72faeb588dfd39d556802c03e7e7d
Dvget <Dvget@gmx.de>
2026-09-03 19:10:58 +0200

    test: register native running metrics

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .target(
             name: "LockedInAnalytics",
             path: "LockedIn",
-            sources: ["TrackingAnalytics.swift", "PolarRunPayload.swift"]
+            sources: ["TrackingAnalytics.swift", "PolarRunPayload.swift", "RunTrackingCore.swift"]
         ),
         .testTarget(
             name: "LockedInAnalyticsTests",
